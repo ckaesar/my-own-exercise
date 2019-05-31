95c3e43ee705f87863f24c32eeb89de9081fcd04
chengk <a50****083@126.com>
2019-06-01 00:18:27 +0800

    [algs4][add examples]

--- src/main/java/com/kaesar/test/HelloWorld.java
+++ src/main/java/com/kaesar/test/HelloWorld.java
@@ -2,6 +2,10 @@ package com.kaesar.test;
 
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world!!!");
+    System.out.println(add(1, 2));
+  }
+
+  public static int add(Integer a, Integer b) {
+    return a + b;
   }
 }
